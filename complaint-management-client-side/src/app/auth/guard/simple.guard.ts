1b04cea5aeebb9264a31b1995e83d9df9374e4a2
basilsaleem <basilsaleem@live.com>
2021-07-27 10:56:33 +0300

    Adding a simple guard for page navigation

---
+++ complaint-management-client-side/src/app/auth/guard/simple.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from '../service/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SimpleGuard implements CanActivate{
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authService.IsAdmin()){
+      this.router.navigate(['/admin']);
+      return false;
+    }else if (this.authService.IsAuthenticated()){
+      this.router.navigate(['user']);
+      return false;
+    }
+    return true;
+  }
+}
